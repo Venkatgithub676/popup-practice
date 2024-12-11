4c99fbde4d984d64ec1932291d3072b94745a626
Venkat <venkatgithub676@gmail.com>
2024-12-11 08:40:05 +0000

    Last Commit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,17 @@
 // Write your code here
+import './index.css'
+
 const About = () => (
-  <div>
+  <div className="about-con">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-sm-img.png"
+      alt="about"
+      className="mobile-about-img"
+    />
     <img
       src="https://assets.ccbp.in/frontend/react-js/about-lg-img.png"
       alt="about"
+      className="desktop-about-img"
     />
   </div>
 )
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,17 @@
 // Write your code here
+import './index.css'
 
 const Home = () => (
-  <div>
+  <div className="home-con">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-sm-img.png"
+      alt="home"
+      className="mobile-home-img"
+    />
     <img
       src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
       alt="home"
+      className="desktop-home-img"
     />
   </div>
 )
